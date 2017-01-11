d7f99c77e0d32ced5d54f9e805291226dc4d0dd6
yuenhsi <yuenhsi93@gmail.com>
2017-01-11 11:39:39 -0800

    generate NSManaged classes

---
+++ dream-lister/Item+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Item+CoreDataClass.swift
+//  dream-lister
+//
+//  Created by Yuen Hsi Chang on 1/11/17.
+//  Copyright © 2017 Yuen Hsi Chang. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+
+public class Item: NSManagedObject {
+
+}
